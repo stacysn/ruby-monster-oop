c93722c5b12976bf0298cd2e6604403f57096ed7
stacysn <stacysuen5@gmail.com>
2017-08-20 18:01:32 -0700

    created threat_level default values

--- starter-code/monster.rb
+++ starter-code/monster.rb
@@ -1,3 +1,39 @@
+class Monster
+  attr_accessor :habitat, :threat_level
+  @@count = 0
+
+  def initialize(threat_level= :medium)
+    @threat_level = threat_level
+    @@count = @@count + 1
+
+    if @@count == 1
+      p "#{@@count} monster now"
+    else
+      p "#{@@count} monsters now"
+    end
+
+    if threat_level == :low || threat_level == :medium || threat_level == :high || threat_level == :midnight
+      @threat_level = threat_level
+    else raise "#{threat_level} is not a valid threat (RuntimeError)"
+    end
+
+  end
+
+  def habitat?(habitat)
+    self.habitat == habitat
+  end
+
+  def self.count
+    @@count
+  end
+
+  p "RAWR! "
+end
+
+p voldemort = Monster.new(:friendly)
+
+
+
 ### monster.rb - run this file
 
 # Monster class
@@ -10,11 +46,11 @@
 
 # Werewolf class
 
- 
+
 
 # Flying module
 
-  
+
 
 # Vampire class
 
@@ -22,4 +58,3 @@
 
 
 ### "Driver" Code Area
-
